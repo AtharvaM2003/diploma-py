e24460f8ebefc77379f99580233c53def0bf99e3
AtharvaM2003 <atharva2mundhe@gmail.com>
2023-09-28 02:29:42 +0530

    p

--- SL/tempCodeRunnerFile.py
+++ SL/tempCodeRunnerFile.py
@@ -1,20 +1,55 @@
-# Selection sort in Python
-# time complexity O(n*n)
-#sorting by finding min_index
-def selectionSort(array, size):
-	
-	for ind in range(size):
-		min_index = ind
+# Python3 code for the above approach
 
-		for j in range(ind + 1, size):
-			# select the minimum element in every iteration
-			if array[j] < array[min_index]:
-				min_index = j
-		# swapping the elements to sort the array
-		(array[ind], array[min_index]) = (array[min_index], array[ind])
+# function to schedule the jobs take 2
+# arguments array and no of jobs to schedule
 
-arr = [-2, 45, 0, 11, -9,88,-97,-202,747]
-size = len(arr)
-selectionSort(arr, size)
-print('The array after sorting in Ascending Order by selection sort is:')
-print(arr)
+def printJobScheduling(arr, t):
+
+	# length of array
+	n = len(arr)
+
+	# Sort all jobs according to
+	# decreasing order of profit
+	for i in range(n):
+		for j in range(n - 1 - i):
+			if arr[j][2] < arr[j + 1][2]:
+				arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+	# To keep track of free time slots
+	result = [False] * t
+
+	# To store result (Sequence of jobs)
+	job = ['-1'] * t
+
+	# Iterate through all given jobs
+	for i in range(len(arr)):
+
+		# Find a free slot for this job
+		# (Note that we start from the
+		# last possible slot)
+		for j in range(min(t - 1, arr[i][1] - 1), -1, -1):
+
+			# Free slot found
+			if result[j] is False:
+				result[j] = True
+				job[j] = arr[i][0]
+				break
+
+	# print the sequence
+	print(job)
+
+
+# Driver's Code
+if __name__ == '__main__':
+	arr = [['a', 2, 100], # Job Array
+			['b', 1, 19],
+			['c', 2, 27],
+			['d', 1, 25],
+			['e', 3, 15]]
+
+
+	print("Following is maximum profit sequence of jobs")
+
+	# Function Call
+	printJobScheduling(arr, 3)
+
